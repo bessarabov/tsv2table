6ccfe6e83ffd97a0715d767c1c28313072debb70
Ivan Bessarabov <ivan@bessarabov.ru>
2022-11-06 02:04:55 +0300

    Some basic work

--- tsv2table/main.go
+++ tsv2table/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("tsv2table")
+
+	var help bool
+	var header string
+
+	flag.BoolVar(&help, "help", false, "usage")
+	flag.StringVar(&header, "header", "auto", "usage")
+
+	flag.Parse()
+
+	if help {
+		fmt.Println("help")
+		os.Exit(1)
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+
+		s := strings.Split(scanner.Text(), "\t")
+		fmt.Println(strings.Join(s[:], " | "))
+
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Println(err)
+	}
+
 }
