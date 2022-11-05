5f83165f905148f4e863cb58b59654633448b2d4
Ivan Bessarabov <ivan@bessarabov.ru>
2022-11-06 02:35:45 +0300

    Align strings

--- tsv2table/main.go
+++ tsv2table/main.go
@@ -4,9 +4,9 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"log"
 	"os"
 	"strings"
+	"unicode/utf8"
 )
 
 func main() {
@@ -24,16 +24,43 @@ func main() {
 		os.Exit(1)
 	}
 
+	var fieldsLength []int
+	var tableData [][]string
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 
-		s := strings.Split(scanner.Text(), "\t")
-		fmt.Println(strings.Join(s[:], " | "))
+		elements := strings.Split(scanner.Text(), "\t")
+		tableData = append(tableData, elements)
+
+		if len(fieldsLength) == 0 {
+			for _, el := range elements {
+				fieldsLength = append(fieldsLength, utf8.RuneCountInString(el))
+			}
+		} else {
+			for i, el := range elements {
+				c := utf8.RuneCountInString(el)
+				if c > fieldsLength[i] {
+					fieldsLength[i] = c
+				}
+			}
+		}
 
 	}
 
 	if err := scanner.Err(); err != nil {
-		log.Println(err)
+		fmt.Printf("Invalid input: %s", err)
+	}
+
+	for _, row := range tableData {
+		for i, el := range row {
+			fmt.Print(el)
+			fmt.Print(strings.Repeat(" ", fieldsLength[i]-utf8.RuneCountInString(el)))
+			if i != len(row)-1 {
+				fmt.Print(" | ")
+			}
+		}
+		fmt.Println()
 	}
 
 }
